700344399eaa3144f23695d16c4988ccc3be4368
mdabdulhalim <htmlcssjs2023@gmail.com>
2023-10-27 11:06:46 +0600

    crud completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 
@@ -11,7 +11,6 @@ app.use(express.json()); // this is used to receive body data
 
 // halimzoomit
 // 65zm9RYIMzQVMBP4
-
 
 
 const uri = "mongodb+srv://halimzoomit:65zm9RYIMzQVMBP4@cluster0.robds1t.mongodb.net/?retryWrites=true&w=majority";
@@ -31,15 +30,61 @@ async function run() {
     await client.connect();
 
       // database connection
-      const database = client.db("usersDB");
-      const userCollection = database.collection("users");
+    //   const database = client.db("usersDB");
+    //   const userCollection = database.collection("users");
 
-    // POST API
+    const userCollection = client.db("usersDB").collection("users");
+
+    //get data, Read data = R
+    app.get('/users', async(req, res)=>{
+        const cursor = userCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // update get
+    app.get('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    })
+
+    // update information
+    app.put('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const user = req.body;
+      // console.log(user);
+
+      // Save data into database
+      const filter ={_id: new ObjectId(id)}
+      const options = {upsert:true};
+      const updatedUser = {
+        $set:{
+          name:user.name,
+          email:user.email
+        }
+      }
+
+      const result = await userCollection.updateOne(filter, updatedUser, options);
+      res.send(result);
+    })
+
+    // POST API - Create data = C
     app.post('/users', async(req, res)=>{
         const user = req.body;
         console.log('new user : ', user);
         // Insert data into  collection
         const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+    // Delete data from database
+    app.delete('/users/:id', async(req, res)=>{
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await userCollection.deleteOne(query);
         res.send(result);
     })
 
